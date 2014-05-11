f90dc45e99d0d2eb9e530ef7aa3709300568c744
bigheadbro <guichaoqun@gmail.com>
2014-05-12 00:05:45 +0800

    5.12

--- src/main/java/com/psylife/entity/UserEntity.java
+++ src/main/java/com/psylife/entity/UserEntity.java
@@ -16,6 +16,29 @@ public class UserEntity implements Serializable {
 	private String domicile;
 	private String school;
 	private String major;
+	private String industry;
+	private String position;
+	private int salary;
+	private int welfare;
+	private int vacation;
+	private int freedom;
+	private int burden;
+	private int development;
+	private int training;
+	private int wtrip;
+	private int atmosphere;
+	private int brand;
+	private int distance;
+	private int lowSalary;
+	private int expectedSalary;
+	private int stateown;
+	private int institution;
+	private int administration;
+	private int foreign;
+	private int privat;
+	private int startup;
+	private int ngo;
+	private int commonweal;
 	
 
 	/**
@@ -115,6 +138,328 @@ public class UserEntity implements Serializable {
 	 */
 	public void setMajor(String major) {
 		this.major = major;
+	}
+
+	/**
+	 * @return the industry
+	 */
+	public String getIndustry() {
+		return industry;
+	}
+
+	/**
+	 * @param industry the industry to set
+	 */
+	public void setIndustry(String industry) {
+		this.industry = industry;
+	}
+
+	/**
+	 * @return the position
+	 */
+	public String getPosition() {
+		return position;
+	}
+
+	/**
+	 * @param position the position to set
+	 */
+	public void setPosition(String position) {
+		this.position = position;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public int getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	/**
+	 * @return the welfare
+	 */
+	public int getWelfare() {
+		return welfare;
+	}
+
+	/**
+	 * @param welfare the welfare to set
+	 */
+	public void setWelfare(int welfare) {
+		this.welfare = welfare;
+	}
+
+	/**
+	 * @return the vacation
+	 */
+	public int getVacation() {
+		return vacation;
+	}
+
+	/**
+	 * @param vacation the vacation to set
+	 */
+	public void setVacation(int vacation) {
+		this.vacation = vacation;
+	}
+
+	/**
+	 * @return the freedom
+	 */
+	public int getFreedom() {
+		return freedom;
+	}
+
+	/**
+	 * @param freedom the freedom to set
+	 */
+	public void setFreedom(int freedom) {
+		this.freedom = freedom;
+	}
+
+	/**
+	 * @return the burden
+	 */
+	public int getBurden() {
+		return burden;
+	}
+
+	/**
+	 * @param burden the burden to set
+	 */
+	public void setBurden(int burden) {
+		this.burden = burden;
+	}
+
+	/**
+	 * @return the development
+	 */
+	public int getDevelopment() {
+		return development;
+	}
+
+	/**
+	 * @param development the development to set
+	 */
+	public void setDevelopment(int development) {
+		this.development = development;
+	}
+
+	/**
+	 * @return the training
+	 */
+	public int getTraining() {
+		return training;
+	}
+
+	/**
+	 * @param training the training to set
+	 */
+	public void setTraining(int training) {
+		this.training = training;
+	}
+
+	/**
+	 * @return the wtrip
+	 */
+	public int getWtrip() {
+		return wtrip;
+	}
+
+	/**
+	 * @param wtrip the wtrip to set
+	 */
+	public void setWtrip(int wtrip) {
+		this.wtrip = wtrip;
+	}
+
+	/**
+	 * @return the atmosphere
+	 */
+	public int getAtmosphere() {
+		return atmosphere;
+	}
+
+	/**
+	 * @param atmosphere the atmosphere to set
+	 */
+	public void setAtmosphere(int atmosphere) {
+		this.atmosphere = atmosphere;
+	}
+
+	/**
+	 * @return the brand
+	 */
+	public int getBrand() {
+		return brand;
+	}
+
+	/**
+	 * @param brand the brand to set
+	 */
+	public void setBrand(int brand) {
+		this.brand = brand;
+	}
+
+	/**
+	 * @return the distance
+	 */
+	public int getDistance() {
+		return distance;
+	}
+
+	/**
+	 * @param distance the distance to set
+	 */
+	public void setDistance(int distance) {
+		this.distance = distance;
+	}
+
+	/**
+	 * @return the lowSalary
+	 */
+	public int getLowSalary() {
+		return lowSalary;
+	}
+
+	/**
+	 * @param lowSalary the lowSalary to set
+	 */
+	public void setLowSalary(int lowSalary) {
+		this.lowSalary = lowSalary;
+	}
+
+	/**
+	 * @return the expectedSalary
+	 */
+	public int getExpectedSalary() {
+		return expectedSalary;
+	}
+
+	/**
+	 * @param expectedSalary the expectedSalary to set
+	 */
+	public void setExpectedSalary(int expectedSalary) {
+		this.expectedSalary = expectedSalary;
+	}
+
+	/**
+	 * @return the stateown
+	 */
+	public int getStateown() {
+		return stateown;
+	}
+
+	/**
+	 * @param stateown the stateown to set
+	 */
+	public void setStateown(int stateown) {
+		this.stateown = stateown;
+	}
+
+	/**
+	 * @return the institution
+	 */
+	public int getInstitution() {
+		return institution;
+	}
+
+	/**
+	 * @param institution the institution to set
+	 */
+	public void setInstitution(int institution) {
+		this.institution = institution;
+	}
+
+	/**
+	 * @return the administration
+	 */
+	public int getAdministration() {
+		return administration;
+	}
+
+	/**
+	 * @param administration the administration to set
+	 */
+	public void setAdministration(int administration) {
+		this.administration = administration;
+	}
+
+	/**
+	 * @return the foreign
+	 */
+	public int getForeign() {
+		return foreign;
+	}
+
+	/**
+	 * @param foreign the foreign to set
+	 */
+	public void setForeign(int foreign) {
+		this.foreign = foreign;
+	}
+
+	/**
+	 * @return the privat
+	 */
+	public int getPrivat() {
+		return privat;
+	}
+
+	/**
+	 * @param privat the privat to set
+	 */
+	public void setPrivat(int privat) {
+		this.privat = privat;
+	}
+
+	/**
+	 * @return the startup
+	 */
+	public int getStartup() {
+		return startup;
+	}
+
+	/**
+	 * @param startup the startup to set
+	 */
+	public void setStartup(int startup) {
+		this.startup = startup;
+	}
+
+	/**
+	 * @return the ngo
+	 */
+	public int getNgo() {
+		return ngo;
+	}
+
+	/**
+	 * @param ngo the ngo to set
+	 */
+	public void setNgo(int ngo) {
+		this.ngo = ngo;
+	}
+
+	/**
+	 * @return the commonweal
+	 */
+	public int getCommonweal() {
+		return commonweal;
+	}
+
+	/**
+	 * @param commonweal the commonweal to set
+	 */
+	public void setCommonweal(int commonweal) {
+		this.commonweal = commonweal;
 	}
 	
 }
